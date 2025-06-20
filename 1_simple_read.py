bfe3200501acb06d27258691a21109c67425ab28
Luke Gutman <lgutman@victaulic.com>
2025-06-20 09:07:47 -0400

    First

---
+++ 1_simple_read.py
@@ -0,0 +1,14 @@
+"""
+The simplest example of reading a tag from a PLC
+
+NOTE: You only need to call .Close() after you are done exchanging
+data with the PLC.  If you were going to read in a loop or read
+more tags, you wouldn't want to call .Close() every time.
+"""
+from pylogix import PLC
+
+comm = PLC()
+comm.IPAddress = '192.168.1.210'
+ret = comm.Read('CurrentScreen')
+print(ret.TagName, ret.Value, ret.Status)
+comm.Close()
